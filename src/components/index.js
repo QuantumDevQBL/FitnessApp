f9bbe141c8b8d2ea147f2f3fe099044ce2ea0a35
quantumDev <tests8q@gmail.com>
2022-07-21 07:42:38 +0200

    add readme desc

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Exercises from './Exercises'
+import HeroBanner from './HeroBanner'
+import SearchExercises from './SearchExercises'
+import Footer from './Footer'
+import Navbar from './Navbar'
+import HorizontalScrollbar from './HorizontalScrollbar'
+
+export {
+  Exercises,
+  HeroBanner,
+  SearchExercises,
+  Footer,
+  Navbar,
+  HorizontalScrollbar
+};
